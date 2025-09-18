c54d9b973e2121da45f40b4d82951dc3b0f7d0ab
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-18 16:32:10 +0530

    added construct

--- 5. Generic Tree/_GenericTreeIntro.java
+++ 5. Generic Tree/_GenericTreeIntro.java
@@ -2,45 +2,75 @@ import java.util.*;
 
 public class _GenericTreeIntro {
 
-  public static class Node{
+  public static class Node {
     int data;
-    ArrayList <Node> children = new ArrayList<>();
+    ArrayList<Node> children = new ArrayList<>();
 
-    Node(int data){
+    Node(int data) {
       this.data = data;
     }
   }
 
-  
+  static void levelOrder(Node root) {
+    Queue<Node> queue = new ArrayDeque<Node>();
+    queue.add(root);
+    while (queue.size() > 0) {
+      Node val = queue.remove();
+      System.out.print(val.data + " ");
+      for (Node child : val.children) {
+        queue.add(child);
+      }
+    }
+    System.out.println();
+  }
+
+  static void levelOrderLinewise(Node root){
+    Queue<Node> queue = new ArrayDeque<Node>();
+    Queue<Node> cqueue = new ArrayDeque<Node>();
+    queue.add(root);
+    while (queue.size() > 0) {
+      Node abc = queue.remove();
+      System.out.print(abc.data + " ");
+      for(Node child: abc.children){
+        cqueue.add(child);
+      }
+
+      if(queue.size() == 0){
+        queue = cqueue;
+        cqueue = new ArrayDeque<Node>();
+        System.out.println();
+      }
+    }
+  }
+
+  static Node construct(int[] arr) {
+    Node root = null;
+    Stack<Node> stack = new Stack<>();
+    for (int val : arr) {
+      if (val != -1) {
+        Node abc = new Node(val);
+        stack.push(abc);
+      } else {
+        Node def = stack.pop();
+        if(stack.size() > 0){
+          Node parent = stack.peek();
+          parent.children.add(def);
+        }
+        else{
+          root = def;
+        }
+      }
+
+    }
+    return root;
+  }
 
   public static void main(String[] args) {
-    Node root = new Node(10); // value daal paa rhe hai because of constructor
-    Node twenty = new Node(20);
-    Node thirty = new Node(30);
-    Node forty = new Node(40);
-    Node fifty = new Node(50);
-    Node sixty = new Node(60);
-    Node seventy = new Node(70);
-    Node eighty = new Node(80);
-    Node hundredten = new Node(110);
-    Node hundred = new Node(100);
-    Node ninety = new Node(90);
-    Node hundredtwenty = new Node(120);
-    
-    root.children.add(twenty);
-    root.children.add(thirty);
-    root.children.add(forty);
-    twenty.children.add(fifty);
-    twenty.children.add(sixty);
-    thirty.children.add(seventy);
-    thirty.children.add(eighty);
-    thirty.children.add(ninety);
-    forty.children.add(hundred);
-    eighty.children.add(hundredten);
-    eighty.children.add(hundredtwenty);
+    int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1 };
+    Node root = construct(arr);
 
     // levelOrder(root);
-    // levelOrderLinewise(root);
+    levelOrderLinewise(root);
   }
 
 }
